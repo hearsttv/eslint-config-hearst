f371b1e814c4ba06b7ecef5ed12b5be851359cfe
Chris O'Donnell <cod@codfish.io>
2017-11-14 10:53:53 -0500

    CCR-928 Add helpful inline comments

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'airbnb-base',
-    // prettier config overwrites any conflicting rules in airbnb's config
+    // prettier config override's any conflicting rules in airbnb's config
     'prettier',
   ],
   env: {
@@ -16,12 +16,17 @@ module.exports = {
   globals: {
     sinon: true,
   },
+  // when running `eslint --fix` this plugin steps in and runs prettier on the
+  // code first, then it runs `eslint --fix`
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': [
       'error',
       {
-        // certain rules that we want to line up with airbnb we specify here
+        // override prettier's default rules that we want to line up with airbnb
+        // instead. airbnb style should be followed as closely as possible.
+        // We're leveraging the added formatting power of prettier rather than
+        // rules like this.
         bracketSpacing: true,
         singleQuote: true,
         semi: true,
@@ -31,13 +36,15 @@ module.exports = {
 
     // hearst specific
     'import/no-amd': 'off', // allow amd
-    'no-bitwise': ['error', { allow: ['~'] }], // allow ~ bitwise char, for int casting
+    'no-bitwise': ['error', { allow: ['~'] }], // allow ~ for integer casting
   },
   overrides: [
     {
+      // add a common override assuming test files are in `/test` dir
+      // and that the application is using chai's assertion library.
+      // allows test statements like `expect(foo).to.be.null;` to pass
       files: ['test/**/*.js'],
       rules: {
-        // allow test statements like `expect(grid.foo).to.be.null;` to pass
         'no-unused-expressions': 'off',
       },
     },
